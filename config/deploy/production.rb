b91eaf273435fcec1957b39d4e2bf2fffae26bef
Mathias Kaufmann <me@stei.gr>
2014-09-16 11:48:14 +0200

    Set Puma-Environment to current deployment stage
    Signed-off-by: Mathias Kaufmann <me@stei.gr>

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,7 @@
 set :branch, 'develop'
 
 set :bundle_without, 'development test'
-set :puma_env, fetch(:rack_env, fetch(:rails_env, fetch(:application,'staging')))
+set :puma_env, fetch(:stage)
 set :puma_threads, [2, 2]
 set :puma_workers, 2
 
