ad938680a5495d0eb5c6dd73aeac475ba5395771
Mathias Kaufmann <me@stei.gr>
2014-09-22 19:40:56 +0200

    Words split by split-characters.
    Do not rely on that awkward \w-thing :-/
    
    Signed-off-by: Mathias Kaufmann <me@stei.gr>

--- app/helpers/quotes_helper.rb
+++ app/helpers/quotes_helper.rb
@@ -6,14 +6,14 @@ module QuotesHelper
 		word =~ /^#(\w+)/
 	end
 	def tag_html word
-		tag = word.scan(/#(\w+)/)[0][0]
+		tag = word.scan(/#([^:, ]+)/)
 		word.sub("##{tag}","<tag><a href='/tag/#{tag}'>##{tag}</a></tag>")
 	end
 	def is_nickname word
 		word =~ /^@(\w+)/ ? true : false
 	end
 	def nickname_html word
-		nickname = word.scan(/@(\w+)/)[0][0]
+		nickname = word.scan(/@([^:, ]+)/)
 		word.sub("@#{nickname}","<nickname><a href='/by/#{nickname}'>@#{nickname}</a></nickname>")
 	end
 end
