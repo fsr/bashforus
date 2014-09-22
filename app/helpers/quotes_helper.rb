ea69ae46061478d8fec1fff108083dad3c4123f7
Mathias Kaufmann <me@stei.gr>
2014-09-22 19:37:00 +0200

    Wrong position for bugfix
    Signed-off-by: Mathias Kaufmann <me@stei.gr>

--- app/helpers/quotes_helper.rb
+++ app/helpers/quotes_helper.rb
@@ -7,13 +7,13 @@ module QuotesHelper
 	end
 	def tag_html word
 		tag = word.scan(/#(\w+)/)[0][0]
-		"<tag><a href='/tag/#{tag}'>##{tag}</a></tag>"
+		word.sub("##{tag}","<tag><a href='/tag/#{tag}'>##{tag}</a></tag>")
 	end
 	def is_nickname word
 		word =~ /^@(\w+)/ ? true : false
 	end
 	def nickname_html word
 		nickname = word.scan(/@(\w+)/)[0][0]
-		"<nickname><a href='/by/#{nickname}'>@#{nickname}</a></nickname>"
+		word.sub("##{nickname}","<nickname><a href='/by/#{nickname}'>@#{nickname}</a></nickname>")
 	end
 end
