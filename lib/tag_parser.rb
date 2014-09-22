2a08a34ee36065846936c1148b8da7804085ad53
Mathias Kaufmann <me@stei.gr>
2014-09-22 19:41:54 +0200

    Updated tag and source-parser too
    Signed-off-by: Mathias Kaufmann <me@stei.gr>

--- lib/tag_parser.rb
+++ lib/tag_parser.rb
@@ -1,7 +1,7 @@
 class TagParser < ActsAsTaggableOn::GenericParser
   def parse
     ActsAsTaggableOn::TagList.new.tap do |tag_list|
-      tag_list.add @tag_list.scan(/#([\w]+)/).flatten
+      tag_list.add @tag_list.scan(/#([^:, ]+)/).flatten
     end
   end
 end
