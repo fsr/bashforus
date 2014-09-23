07bec9a09df933dbb7efd0273d01e8d9865111d7
Mathias Kaufmann <me@stei.gr>
2014-09-23 18:48:25 +0200

    Make nickname and tag format configurable
    Signed-off-by: Mathias Kaufmann <me@stei.gr>

--- app/helpers/quotes_helper.rb
+++ app/helpers/quotes_helper.rb
@@ -6,14 +6,14 @@ module QuotesHelper
 		word =~ /^#(\w+)/
 	end
 	def tag_html word
-		tag = word.scan(/#([^:, \.]+)/)[0][0]
+		tag = word.scan(Regexp.new(CONFIG['tag_format'].strip))[0][0]
 		word.sub("##{tag}","<tag><a href='#{Rails.application.routes.url_helpers.tag_url(id: tag, host: CONFIG['domain'],subdomain: ( @channel || channel).subdomain)}'>##{tag}</a></tag>")
 	end
 	def is_nickname word
 		word =~ /^@(\w+)/ ? true : false
 	end
 	def nickname_html word
-		nickname = word.scan(/@([^:, \.]+)/)[0][0]
+		nickname = word.scan(Regexp.new(CONFIG['nickname_format'].strip))[0][0]
 		word.sub("@#{nickname}","<nickname><a href='#{Rails.application.routes.url_helpers.by_url(id: nickname, host: CONFIG['domain'],subdomain:  ( @channel || channel).subdomain)}'>@#{nickname}</a></nickname>")
 	end
 end
--- lib/tag_parser.rb
+++ lib/tag_parser.rb
@@ -1,7 +1,7 @@
 class TagParser < ActsAsTaggableOn::GenericParser
   def parse
     ActsAsTaggableOn::TagList.new.tap do |tag_list|
-      tag_list.add @tag_list.scan(/#([^:, \.]+)/).flatten.uniq
+      tag_list.add @tag_list.scan(Regexp.new(CONFIG['tag_format'].strip)).flatten.uniq
     end
   end
 end
