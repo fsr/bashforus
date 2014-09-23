81927f51a11a87c3d8bfd7e331a02a3de70187fa
Mathias Kaufmann <me@stei.gr>
2014-09-23 19:09:36 +0200

    Bugfix private.yml
    Signed-off-by: Mathias Kaufmann <me@stei.gr>

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,11 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 CONFIG  = YAML.load_file("#{Rails.root}/config/config.yml")[Rails.env]
-PRIVATE = File.exists?("#{Rails.root}/config/private.yml") ? YAML.load_file("#{Rails.root}/config/private.yml")[Rails.env] : {}
+PRIVATE = if File.exists?("#{Rails.root}/config/private.yml")
+	YAML.load_file("#{Rails.root}/config/private.yml")[Rails.env]
+else
+	{}
+end
 
 # Initialize the Rails application.
 Rails.application.initialize!
